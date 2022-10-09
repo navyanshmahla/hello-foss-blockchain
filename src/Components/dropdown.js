f81c1a6b91939521863f2970b941ab4e43a994e5
Navyansh Mahla <navyanshmahla@Navyanshs-MacBook-Air.local>
2022-10-09 23:45:14 +0530

    added hovering animations

--- src/Components/dropdown.js
+++ src/Components/dropdown.js
@@ -1,6 +1,17 @@
 import Dropdown from 'react-bootstrap/Dropdown';
+import {useState} from 'react';
+
 
 function BasicExample() {
+    const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseOut = () => {
+    setIsHovering(false);
+  };
   return (
     <Dropdown>
       <Dropdown.Toggle variant="success" id="dropdown-basic">
@@ -8,9 +19,9 @@ function BasicExample() {
       </Dropdown.Toggle>
 
       <Dropdown.Menu>
-        <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-        <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+        <Dropdown.Item href="#/action-1">Safe</Dropdown.Item>
+        <Dropdown.Item href="#/action-2"onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>Big Day {isHovering && <h2>Coming soon!</h2>}</Dropdown.Item>
+        <Dropdown.Item href="#/action-3"onMouseOver={handleMouseOver} onMouseOut={handleMouseOut}>Countdown {isHovering && <h2>Coming soon!</h2>}</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
   );
---
+++ src/Components/safe.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+
+export default function safe() {
+  return (
+    <div>safe</div>
+    
+  )
+}
