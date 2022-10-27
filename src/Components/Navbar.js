fef3c23d561d8a62a2da087d1d6f573622e10e25
Karrthik Arya <200020068@iitb.ac.in>
2022-10-27 17:09:29 +0530

    nav issue ressolved

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+// import NavDropdown from 'react-bootstrap/NavDropdown';
 import logo from '../resources/wncc_logo_white.png';
-import {Link} from "react-router-dom";
+// import {Link} from "react-router-dom";
 export default class 
  extends Component {
   render() {
@@ -16,8 +16,8 @@ export default class
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link Link to="/">Home</Nav.Link>
-            <Nav.Link Link to="/dapp">dApp</Nav.Link>
+            <Nav.Link href="/">Home</Nav.Link>
+            <Nav.Link href="/dapp">dApp</Nav.Link>
             
           </Nav>
         </Navbar.Collapse>
