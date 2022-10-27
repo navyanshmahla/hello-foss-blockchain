b7b02ee1823366187a8ec859296cba95dfb69166
Karrthik Arya <200020068@iitb.ac.in>
2022-10-27 18:18:33 +0530

    friend list added

--- contracts/Lock.sol
+++ contracts/Lock.sol
@@ -5,9 +5,15 @@ contract SolidityTest
    uint my_timestamp;  // after how many time the time capsule will open
    string my_info;   // info of time capsule here it is string which we will improve
    address makers_address; // time capsule makers address
+   mapping(address => bool) friends;
 
    modifier check_address() {
-      require (msg.sender == makers_address);  // only maker can access the time capsule
+      require (friends[msg.sender]);  // only maker can access the time capsule
+         _;
+   }
+
+   modifier is_owner() {
+      require (msg.sender==makers_address); 
          _;
    }
 
@@ -24,7 +30,14 @@ contract SolidityTest
 
    function recieve_data (string memory info) public{
       my_info = info ;  // recieve info from frontend
+      makers_address = msg.sender;
+      friends[makers_address] = true;
    } 
+
+   function add_friend (address friend_addr) public is_owner {
+       friends[friend_addr] = true;
+   }
+   
    function show_data () public view correct_time check_address returns(string memory info)  {
       info = my_info;  // returns the info 
    }
